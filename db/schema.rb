b27eef8c8b1a9595527ceffbef1af374ca4c96f2
Marco Galanti <marco.galanti@gmail.com>
2018-02-06 22:27:58 +0100

    paperclip configuration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202104237) do
+ActiveRecord::Schema.define(version: 20180203003811) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "first_name", default: "", null: false
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20180202104237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "group", default: "", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
